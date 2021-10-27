55cf5dd867e68c97b8ca6dba7fdb64fff43f1bb5
Nicolas Saldivar <nicolasmsaldivar@gmail.com>
2021-10-26 23:40:54 -0300

    agrege un uso de DOM y evento

--- main.js
+++ main.js
@@ -39,11 +39,18 @@ myShoppingCart.add(product1);
 myShoppingCart.add(product2);
 
 let contentShoppingCart = myShoppingCart.get();
-let filterProduct = myShoppingCart.getById(4);
+let filterProduct = myShoppingCart.getById(1);
 if(filterProduct.length>0){
 console.log(filterProduct)
 }else{
     console.log('error');
 }
 
+//DOM y Evento
+let click = document.getElementById('agregar');
+click.addEventListener('click',()=>{
+    let compra = document.createElement("p");
+    compra.innerHTML = `"<h3>Acabas de agregar al carrito ${product1.name} y sale $ ${product1.price}</h3>"`
+    document.body.appendChild(compra)
+})
 
