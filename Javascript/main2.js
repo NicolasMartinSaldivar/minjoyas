59aebea224402c5deae35f5717c43e9829ac893c
Nicolas Saldivar <nicolasmsaldivar@gmail.com>
2021-11-24 23:31:02 -0300

    modificaciones

--- Javascript/main2.js
+++ Javascript/main2.js
@@ -1,77 +1,69 @@
-let product1 = new Product(1,"anillo 1 ",500,"anillo de oro");
-let product2 = new Product(2,"anillo 2 ",400,"anillo de plata");
+// $(document).ready(()=>{
+//     for(let i=0; i<arregloProduct.length;i++){
+//         let objetoProduct = arregloProduct[i];
+//         $('#carrito').append(
+//             `<div id="div${objetoProduct.getId()} ">
+//             <h2>${objetoProduct.getTitle()}</h2>
+//             <p>${objetoProduct.description}</p>
+//             <button id="btn${objetoProduct.getId()}">Compralo!</button>
+//             </div>       
+//             `
+//         );
+
+//         $(`#btn${objetoProduct.getId()}`).on('click',function(){
+//             alert(`Compra ${objetoProduct.getName()}`);
+
+//         $(`#div${objetoProduct.getId()}`).fadeOut(1000);
+//         })
+//     }
+// })
+
+// $(document).ready(function(){
+//     $('#botonEnviar').click(function(){
+//         if(validarForm()){
+//             setTimeout(()=>{
+//                 let resultado = Math.floor(Math.random()*2);
+//                 mostrarMensaje(res);
+//             },2000)
+
+//         }
 
 
-let arregloProduct = new Array();
-arregloProduct.push(product1);
-arregloProduct.push(product2);
+//     })
+// })
+// function mostrarMensaje(res){
+//     $('#formulario').fadeOut("slow");
+//     if(res ==0){
+//         $('#exito').delay(500).fadeIn("slow");
+//     }else{
+//         $('#fracaso').delay(500).fadeIn("slow");
+//     }
+// }
 
-$(document).ready(()=>{
-    for(let i=0; i<arregloProduct.length;i++){
-        let objetoProduct = arregloProduct[i];
-        $('#carrito').append(
-            `<div id="div${objetoProduct.getId()} ">
-            <h2>${objetoProduct.getTitle()}</h2>
-            <p>${objetoProduct.description}</p>
-            <button id="btn${objetoProduct.getId()}">Compralo!</button>
-            </div>       
-            `
-        );
+// function validarForm(){
+//     if($('#nombre').val() == ''){
+//         alert('El campo nombre no puede estar vacío');
+//         $('#nombre').focus();
+//         return false;
+//     }
+//     if($('#celular').val()==''){
+//         alert('El campo celular no puede estar vacío');
+//         $('#celular').focus();
+//         return false;
+//     }
+//     if($('#correo').val()==''){
+//         alert('El campo correo no puede estar vacío');
+//         return false;
+//     }
 
-        $(`#btn${objetoProduct.getId()}`).on('click',function(){
-            alert(`Compra ${objetoProduct.getName()}`);
+//     return false;
 
-        $(`#div${objetoProduct.getId()}`).fadeOut(1000);
-        })
-    }
-})
-
-$(document).ready(function(){
-    $('#botonEnviar').click(function(){
-        if(validarForm()){
-            setTimeout(()=>{
-                let resultado = Math.floor(Math.random()*2);
-                mostrarMensaje(res);
-            },2000)
-
-        }
-
-
-    })
-})
-function mostrarMensaje(res){
-    $('#formulario').fadeOut("slow");
-    if(res ==0){
-        $('#exito').delay(500).fadeIn("slow");
-    }else{
-        $('#fracaso').delay(500).fadeIn("slow");
-    }
-}
-
-function validarForm(){
-    if($('#nombre').val() == ''){
-        alert('El campo nombre no puede estar vacío');
-        $('#nombre').focus();
-        return false;
-    }
-    if($('#celular').val()==''){
-        alert('El campo celular no puede estar vacío');
-        $('#celular').focus();
-        return false;
-    }
-    if($('#correo').val()==''){
-        alert('El campo correo no puede estar vacío');
-        return false;
-    }
-
-    return false;
-
-}
+// }
 
 
 /*Visibilidad de Guia de pasos*/
 
-jQuery(function(){
+$(function(){
     $("#button").on("click",function(){
         if($("#guia").is(":visible")){
             $("#guia").hide();
--- main.js
+++ main.js
@@ -19,7 +19,7 @@ class ShoppingCart {
     }
 }
 
-class Product {
+class Producto {
     constructor(id, name, price, description) {
         this.id = id;
         this.name = name;
@@ -41,3 +41,47 @@ class Product {
     
 }
 
+$(document).ready(()=>{
+    const ANILLOSJSON = "../Javascript/anillos.json";
+
+    $('.producto').prepend(`<div>
+                            <button class="btn-comprar">Comprar</button>
+                            </div>`)
+    $('.btn-comprar').click(()=>{
+        $.get(ANILLOSJSON,function(respuesta,estado){
+            
+            if(estado = "success"){
+                let misProductos = respuesta;
+                const enJSON =  JSON.stringify(Producto);
+
+                for(const producto of misProductos){
+                    $('.producto').append(`<div class="producto">
+                                            <h3>${producto.name}</h3>
+                                            <img ${producto.image}/>
+                                            <p>${producto.description}</p>
+                                            <div class="precio">${producto.price}</div>
+                                            <button value="Comprar" class="botonComprar">Comprar</button>
+                                            <div class="comprado">Agregado al carrito</div>
+                                            </div>`)
+
+                localStorage.setItem(misProductos, enJSON);
+                }
+            }else{
+                alert("error al hacer get");
+            }
+        })
+    })
+})
+
+//funcion para boton comprar
+
+$(function(){
+    $(".botonComprar").on("click",function(){
+        if($(".comprado").is(":visible")){
+            $(".comprado").hide(3000);
+        }else{
+            $(".comprado").show(8000);
+            
+        }
+    })
+})
